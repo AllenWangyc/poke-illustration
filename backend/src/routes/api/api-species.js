1f3448113a82dbb6af5b498711502668753af3e7
AllenWangyc <451366033@qq.com>
2024-05-24 19:01:38 +1200

    task2 finished

--- backend/src/routes/api/api-species.js
+++ backend/src/routes/api/api-species.js
@@ -1,7 +1,17 @@
 import express from "express";
+import { Species } from "../../data/schema.js";
 
 const router = express.Router();
 
 // TODO Your code here.
+const retrieveAllSpecies = async () => {
+  return await Species.find()
+}
+
+router.get('/', async (req, res) => {
+  const allSpecies = await retrieveAllSpecies()
+  if (allSpecies) return res.json(allSpecies)
+  return res.sendStatus(400)
+})
 
 export default router;
