2e135281645230c36c394329fa09d3d6c5ba619f
AllenWangyc <451366033@qq.com>
2024-05-24 18:21:55 +1200

    task1 finished

--- backend/src/data/schema.js
+++ backend/src/data/schema.js
@@ -4,5 +4,35 @@ import { VALID_TYPES } from "./types.js";
 const Schema = mongoose.Schema;
 
 // TODO Your code here.
+const speciesSchema = new Schema({
+  dexNumber: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  dexEntry: {
+    type: String,
+    required: true
+  },
+  types: [{
+    type: String,
+    enum: VALID_TYPES
+  }]
+}, {
+  timestamps: {}
+})
 
-export { Species };
+speciesSchema.index({ name: "text", dexEntry: "text" })
+
+const Species = mongoose.model('Species', speciesSchema)
+
+export { Species }
